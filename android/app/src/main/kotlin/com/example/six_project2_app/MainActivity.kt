6a957ed26dbb9fce7c02322fae9f1257736797b6
Jumisan <olga.jumi.san@gmail.com>
2023-12-11 02:06:36 +0400

    del
    del

--- android/app/src/main/kotlin/com/example/six_project2_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.six_project2_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/Location.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Location {
-  final String name;
-  final String description;
-  final String pic;
-
-  Location(this.name, this.description, this.pic);
-}
